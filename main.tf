93ec6c96e690eebde91b8b6898b3ecb3f7e618f8
padmarajuraju <111412403+padmarajuraju@users.noreply.github.com>
2023-01-25 07:19:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-east-1"
-  
-  
+  region = "us-east-2"
+  access_key = "AKIAQHLVDM7ARMTFVRYP"
+  secret_key = "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"
 }
 
 #Create a new EC2 launch configuration
